cd35e0a8691313ef929df68e8f44903357281c21
otomakin <tomakintomakin05@gmail.com>
2025-03-14 14:25:18 +0300

    CQRSDesignPattern

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Command/MovieCommands/CreateMovieCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Command.MovieCommands
+{
+    public class CreateMovieCommand
+    {
+        public string Title { get; set; }
+        public string CoverImageUrl { get; set; }
+        public decimal Rating { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; } // Film Kaç Dakika Sürüyor
+        public DateTime ReleaseDate { get; set; } // Film Ne Zaman Yayınlandı
+        public string CreatedDATE { get; set; } // Film Veritabanına Ne Zaman Eklendi
+        public bool Status { get; set; } // Fim Aktif Olarak Sitede Var  mı
+    }
+}
